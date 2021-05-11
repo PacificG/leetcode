3cb140e221dca92c2aeb25e9ad927f272ed3515e
1pk73 <Prashantkkumar1998@gmail.com>
2021-05-11 22:23:55 +0530

    started leetcode

--- Google/twoSum.py
+++ Google/twoSum.py
@@ -3,6 +3,9 @@ def twoSum(nums: list, target:int):
     Given an array of integers nums and an integer target, return indices of the two numbers 
     such that they add up to target.You may assume that each input would have exactly one solution, 
     and you may not use the same element twice. You can return the answer in any order.
+
+    >>> twoSum([3, 2, 1, 4], 6)
+    [1, 3]
     """
     dic = {i:target - nums[i] for i in range(len(nums))}
     for i in dic:
@@ -16,10 +19,3 @@ def twoSum(nums: list, target:int):
                 if nums.index(diff) != i:
                     return [i, nums.index(target - nums[i])]
     """
-
-
-
-
-
-if __name__ == "__main__":
-    print(twoSum([3, 2, 1, 4], 6))
